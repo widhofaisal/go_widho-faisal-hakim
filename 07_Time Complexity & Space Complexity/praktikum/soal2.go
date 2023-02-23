4eed9f9c155e2ebb14184bccd867a671a15d1e1d
widhofaisal <widhofaisal26@gmail.com>
2023-02-23 17:28:57 +0700

    done : soal bilanganpangkat complexity O(log n)

--- 07_Time Complexity & Space Complexity/praktikum/soal2.go
+++ 07_Time Complexity & Space Complexity/praktikum/soal2.go
@@ -1,27 +1,27 @@
 package main
 
-import("fmt")
+import (
+	"fmt"
+)
 
 func pow(x, n int) int {
-    if n == 0 {
-        return 1
-    } else if n == 1 {
-        return x
-    } else if n%2 == 0 {
-        y := pow(x, n/2)
-        return y * y
-    } else {
-        y := pow(x, (n-1)/2)
-        return x * y * y
-    }
+	if n == 0 {
+		return 1
+	} else if n == 1 {
+		return x
+	} else if n%2 == 0 {
+		y := pow(x, n/2)
+		return y * y
+	} else {
+		y := pow(x, (n-1)/2)
+		return x * y * y
+	}
 }
 
-
-
 func main() {
-   fmt.Println(pow(2, 3))  // 8
-   fmt.Println(pow(5, 3))  // 125
-   fmt.Println(pow(10, 2)) // 100
-   fmt.Println(pow(2, 5))  // 32
-   fmt.Println(pow(7, 3))  // 343
+	fmt.Println(pow(2, 3))  // 8
+	fmt.Println(pow(5, 3))  // 125
+	fmt.Println(pow(10, 2)) // 100
+	fmt.Println(pow(2, 5))  // 32
+	fmt.Println(pow(7, 3))  // 343
 }
