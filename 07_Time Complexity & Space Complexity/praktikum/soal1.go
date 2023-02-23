0f60368e5f526c741e7f98021e09a0794b106b56
widhofaisal <widhofaisal26@gmail.com>
2023-02-23 17:23:34 +0700

    done : soal bilangan prima dengan O(sqrt(n))

--- 07_Time Complexity & Space Complexity/praktikum/soal1.go
+++ 07_Time Complexity & Space Complexity/praktikum/soal1.go
@@ -1,16 +1,30 @@
 package main
 
-import("fmt")
+import (
+	"fmt"
+)
 
-func primeNumber(number int) bool {
+// Buatlah solusi dengan kompleksitas lebih cepat dari O(n) / O(n/2)
+// O(sqrt(n))
+func primeNumber(n int) bool {
+	//    fmt.Println("number", n)
+	if n <= 1 {
+		return false
+	}
+
+	for i := 2; i*i <= n; i++ {
+		//   fmt.Printf("i=%v, i*i=%v \n", i, i*i)
+		if n%i == 0 {
+			return false
+		}
+	}
 	return true
 }
 
-
 func main() {
-   fmt.Println(primeNumber(1000000007)) // true
-   fmt.Println(primeNumber(13))         // true
-   fmt.Println(primeNumber(17))         // true
-   fmt.Println(primeNumber(20))         // false
-   fmt.Println(primeNumber(35))         // false
+	fmt.Println(primeNumber(1000000007)) // true
+	fmt.Println(primeNumber(13))         // true
+	fmt.Println(primeNumber(17))         // true
+	fmt.Println(primeNumber(20))         // false
+	fmt.Println(primeNumber(35))         // false
 }
