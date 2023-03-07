1cd7f5ab9ac74ff7c6671970e55f635152f3d375
widhofaisal <widhofaisal26@gmail.com>
2023-03-08 02:23:12 +0700

    sisa semalam

--- 12_Concurrent Programing/praktikum/main.go
+++ 12_Concurrent Programing/praktikum/main.go
@@ -1,1 +1,37 @@
-package praktikum
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SafeNumber struct {
+	i int
+	m sync.Mutex
+}
+
+func (safeNumber *SafeNumber) Set(number int) {
+	safeNumber.m.Lock()
+	defer safeNumber.m.Unlock()
+	safeNumber.i = number
+}
+
+func (safeNumber *SafeNumber) Get() int {
+	safeNumber.m.Lock()
+	defer safeNumber.m.Unlock()
+	return safeNumber.i
+}
+
+func GetNumber() int {
+	i := SafeNumber{}
+	go func() {
+		i.Set(5)
+	}()
+	time.Sleep(100 * time.Millisecond)
+	return i.Get()
+}
+
+func main() {
+	fmt.Println(GetNumber())
+}
