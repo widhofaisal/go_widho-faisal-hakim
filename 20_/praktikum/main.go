64c5a9f70836fb7cf2089b41de1a8f5271950316
widhofaisal <widhofaisal26@gmail.com>
2023-03-26 15:16:20 +0700

    create simple rest api

--- 20_/praktikum/main.go
+++ 20_/praktikum/main.go
@@ -1,1 +1,19 @@
-package praktikum
+package main
+
+import (
+	"net/http"
+	
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	
+	e.GET("/students", get_students_handler)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func get_students_handler(c echo.Context) error {
+	print(http.StatusOK)
+	return c.String(http.StatusOK, "Hello, World!")
+}
