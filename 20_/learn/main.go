74021c2aa73c879c88cee5ca406d0234a1a587ae
widhofaisal <widhofaisal26@gmail.com>
2023-04-01 20:11:33 +0700

    watch video and resume

---
+++ 20_/learn/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type User struct{
+	Id int `json: "id"`
+	Age int `json: "age"`
+	Email string `json: "email"`
+	Name string `json: "name"`
+}
+
+func main(){
+	e:= echo.New()
+
+	// routing
+	e.GET("/user/:id/:age", UserController)
+	e.POST("/user", RegisterController)
+	e.POST("/bindingjson", BindingController)
+
+	e.Start(":8000")
+}
+
+// response hello world
+func UserController(e echo.Context) error {
+	id, _ :=strconv.Atoi(e.Param("id"))
+	age,_:=strconv.Atoi(e.Param("age"))
+	search:=e.QueryParam("search")
+
+	user:= User{id, age, "joko@gmail.com", "Joko"}
+
+	return e.JSON(http.StatusOK, map[string]interface{}{
+		"user":user,
+		"search":search,
+	})
+	// return e.String(200, "Hello World")	// same
+}
+
+func RegisterController(e echo.Context) error{
+	email:= e.FormValue("email")
+	name:= e.FormValue("name")
+
+	return e.JSON(http.StatusOK, map[string]interface{}{
+		"email":email,
+		"name":name,
+	})
+}
+
+func BindingController(e echo.Context) error{
+	user:=User{}
+	e.Bind(&user)
+
+	return e.JSON(http.StatusOK, map[string]interface{}{
+		"data":user,
+	})
+}
